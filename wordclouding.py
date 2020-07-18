3db57515126f4203a2c06bc24a0644eaf1e63853
Peca Lee <studiopeca.lee@gmail.com>
2020-07-18 20:03:43 +0900

    except_wording

--- wordclouding.py
+++ wordclouding.py
@@ -3,7 +3,20 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
 
+def remove_word_from_dict(words_counted):
+    counted_words_dict = words_counted
+    stopwords = ["ある", "いる", "あ", "い", "の", "する", "ます", "ｗ", "www", "ｗｗｗ"]
+    for stopword in stopwords:
+        try:
+            counted_words_dict.pop(stopword)
+        except:
+            pass
+
+    return counted_words_dict
+
+
 def tabelog_wordcloud(words_counted):
+    keywords = remove_word_from_dict(words_counted)
     font_path = "./font/NotoSansJP-Bold.otf"
     wordcloud = WordCloud(
         font_path=font_path,
@@ -11,7 +24,7 @@ def tabelog_wordcloud(words_counted):
         height=800,
         background_color="white",
     )
-    wordcloud = wordcloud.generate_from_frequencies(words_counted)
+    wordcloud = wordcloud.generate_from_frequencies(keywords)
 
     plt.figure()
     plt.imshow(wordcloud, interpolation="bilinear")
