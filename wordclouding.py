4a5bd805dab070388b71dd5d0450505db40963dc
Peca Lee <studiopeca.lee@gmail.com>
2020-07-18 20:15:54 +0900

    except_wording

--- wordclouding.py
+++ wordclouding.py
@@ -5,7 +5,8 @@ from wordcloud import WordCloud
 
 def remove_word_from_dict(words_counted):
     counted_words_dict = words_counted
-    stopwords = ["ある", "いる", "あ", "い", "の", "する", "ます", "ｗ", "www", "ｗｗｗ"]
+    stopwords = ["ある", "いる", "あ", "い", "の", "する", "ます", "ｗ",
+                 "www", "ｗｗｗ", "れ", "し", "ん", "円", "あり", "／", "ー", "杯", "さ", "枚", ]
     for stopword in stopwords:
         try:
             counted_words_dict.pop(stopword)
@@ -20,8 +21,8 @@ def tabelog_wordcloud(words_counted):
     font_path = "./font/NotoSansJP-Bold.otf"
     wordcloud = WordCloud(
         font_path=font_path,
-        width=800,
-        height=800,
+        width=1200,
+        height=1200,
         background_color="white",
     )
     wordcloud = wordcloud.generate_from_frequencies(keywords)
